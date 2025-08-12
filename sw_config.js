5263c6b7c0925a271d099d34cd9e109e3f0fb7d0
Miolin <miolin.dios@gmail.com>
2025-08-12 19:44:43 +0300

    upload files

--- sw_config.js
+++ sw_config.js
@@ -1,3 +1,3 @@
-const CACHE_NAME = 'pos-cache-v2.51.3-175-1755016674644';
+const CACHE_NAME = 'pos-cache-v2.51.3-175-1755017061264';
 const RESOURCES_PATH_PART = 'composeResources/bossitpos.composeapp.generated.resources';
-const RESOURCES_VERSION_QUERY = '?v=2.51.3-175-1755016674644';
+const RESOURCES_VERSION_QUERY = '?v=2.51.3-175-1755017061264';
